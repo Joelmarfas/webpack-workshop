35f6e4102c89156c99efeb50dfae3efc2076fc4c
Joel <joelmarfas@gmail.com>
2021-09-07 14:44:46 +0200

    workshop Joel

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+// module.exports = {
+//   mode: "development",
+//   entry: {
+//     main: path.resolve(__dirname, "./src/index.js")
+//   },
+//   output: {
+//     path: path.resolve(__dirname, "./dist"),
+//     filename: '[name].bundle.js'
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: "Webpack Workshop Test",
+//       template: path.resolve(__dirname, "./src/index.html"),
+//       filename: "index.html"
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: ["babel-loader"]
+//       }
+//     ]
+//   }
+// }
+
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+
+
+module.exports = {
+
+    mode: "development",
+    devServer: {
+      historyApiFallback: true,
+      port: 4000,
+      open: true,
+      compress: true,
+      hot: true
+    },
+    entry: {
+
+        main: path.resolve(__dirname, "./src/index.js")
+
+    },
+
+    output: {
+
+        path: path.resolve(__dirname, "./dist"),
+
+        filename: "[name].bundle.js"
+
+    },
+
+    plugins: [
+
+        new HtmlWebpackPlugin({
+
+            title: "Webpack Workshop Test",
+
+            template: path.resolve(__dirname, "./src/index.html"),
+
+            filename: "index.html"
+
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+              test: /\.(scss|css)$/,
+              use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    }
+}
